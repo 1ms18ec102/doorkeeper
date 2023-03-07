43defbc9d53561f0d2f72b33c8da3a18750786e5
Savan  Awanti <savan.awanti@happiestminds.com>
2023-03-07 13:34:46 +0530

    solved

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
+  namespace :api do
+    resources :users, only: %i[create]
+    resources :bookmarks, only: %i[index]
+  end
+
+  root 'bookmarks#index'
+end
