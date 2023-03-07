1b2b8171498f5d74ecc86dc1b21389b51b77939d
Savan  Awanti <savan.awanti@happiestminds.com>
2023-03-07 14:54:17 +0530

    fdv

--- app/controllers/api/bookmarks_controller.rb
+++ app/controllers/api/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 module Api
     class BookmarksController < Api::ApplicationController
+      before_action :authenticate_user!
       def index
         @bookmarks = Bookmark.all
         render json: { bookmarks: @bookmarks }
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,3 @@
 class Bookmark < ApplicationRecord
-    before_action :authenticate_user!
+    
 end
